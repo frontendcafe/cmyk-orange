4eced6b597a164a51ddbd0b50a60dedbe44c50a0
Angela <angelakgo20@gmail.com>
2021-09-13 16:25:27 -0300

    cambie estilos

---
+++ js/auth.js
@@ -0,0 +1,16 @@
+// Configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyC_yF5Y90-GsKfO7fZWmS6OQFv5Gj7B8a8",
+  authDomain: "cmyk-orange.firebaseapp.com",
+  projectId: "cmyk-orange",
+  storageBucket: "cmyk-orange.appspot.com",
+  messagingSenderId: "179241068454",
+  appId: "1:179241068454:web:5e1763ec4bbee8ef5d5167",
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+const userEmail = document.querySelector("#email");
+console.log(userEmail);
