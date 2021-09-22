465e4de15abc9100e297f61f846099c335eb2cce
Angela <angelakgo20@gmail.com>
2021-09-22 17:32:37 -0300

    firesbase en la home y una imagen

--- js/main.js
+++ js/main.js
@@ -1,11 +1,1 @@
-firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/firebase.User
-    var uid = user.uid;
-    // ...
-  } else {
-    // User is signed out
-    // ...
-  }
-});
+
