3fe178c209bf893b27a38015a051b578153d88ec
storres20 <sanchotorres777@gmail.com>
2021-09-28 17:51:46 -0500

    about-us.html al 90%

--- js/about-us.js
+++ js/about-us.js
@@ -1,10 +1,1 @@
-/* Popper.js - start */
-const button = document.querySelector('#button');
-  const tooltip = document.querySelector('#tooltip');
 
-  // Pass the button, the tooltip, and some options, and Popper will do the
-  // magic positioning for you:
-  Popper.createPopper(button, tooltip, {
-    placement: 'top',
-  });
-/* Popper.js - end */
