ad29f03c440fd8a895f3b8e39267d9165cfa5297
storres20 <sanchotorres777@gmail.com>
2021-09-28 22:02:22 -0500

    update about-us

--- js/about-us.js
+++ js/about-us.js
@@ -1,1 +1,38 @@
+/************** About us - start ***************/
 
+/* Popper Js - start */
+const button = document.querySelector('.teamBtn');
+const tooltip = document.querySelector('#tooltip');
+
+const popperInstance = Popper.createPopper(button, tooltip, {
+  placement: 'top',
+});
+
+
+function show() {
+  tooltip.setAttribute('data-show', '');
+
+  // We need to tell Popper to update the tooltip position
+  // after we show the tooltip, otherwise it will be incorrect
+  popperInstance.update();
+}
+
+function hide() {
+  tooltip.removeAttribute('data-show');
+}
+
+const showEvents = ['click', 'focus'];
+const hideEvents = ['mouseleave', 'blur'];
+
+showEvents.forEach((event) => {
+  button.addEventListener(event, show);
+});
+
+hideEvents.forEach((event) => {
+  button.addEventListener(event, function(){
+    tooltip.addEventListener(event, hide);
+  });
+});
+/* Popper Js - end */
+
+/************** About us - end ***************/
